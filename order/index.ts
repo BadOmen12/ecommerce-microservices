ed6cac8c2fa95a4aa0dcee8fc69e00dcc9330cfc
Omar <omar.elwakil@vodafone.com>
2022-11-26 18:34:44 +0200

    add docker files

--- order/index.ts
+++ order/index.ts
@@ -1,7 +1,11 @@
-import express from "express";
+import express, { Request, Response } from "express";
 
 const app = express()
 
 const PORT = process.env.PORT || 3000;
+
+app.get('/', (res: Response, req: Request) => {
+    res.send('Order service')
+})
 
 app.listen(PORT, () => console.log(`Product service is up and running in port ${PORT}`))
